6becc70bc230bb20e70180a22c2a98f57f316964
xazi2 <ytapelaxazi@gmail.com>
2025-09-16 16:46:28 -0300

    adicionando alterar contato

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 
     path('add_contato/', views.add_contato, name='add_contato'),
+    path('editarContato/<int:id>', views.editar_contato, name='editar_contato'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from core.models import Contato
+
 
 def add_contato(request):
    return render(request, 'core/add_contato.html')
-
+def editarContato(request, id):
+    contato = get_object_or_404(Contato,id=id)
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        telefone = request.POST.get('telefone')
+        contato.nome = nome
+        contato.sobrenome = sobrenome
+        contato.email = email
+        contato.telefone = telefone
+        contato.save()
+        return redirect('list_contato')
+    context = {
+        'contato': contato
+    }
+    return render(request, 'core/editarContato.html', context)
